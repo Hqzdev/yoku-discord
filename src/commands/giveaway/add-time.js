0058e4fcdd4a6356e6a5cba092c807b46b5096a8
Haqz.dev <105918255+Hqzdev@users.noreply.github.com>
2024-11-24 20:13:16 +0500

    fix bugs

--- src/commands/giveaway/add-time.js
+++ src/commands/giveaway/add-time.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ApplicationCommandOptionType } = require("discord.js");
 
 module.exports = {
     name: "add-time-giveaway",
